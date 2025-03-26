c72474670fdf530138aa8a5f3e3a7af4d337624e
Willy Romão <willyr.goncalves@gmail.com>
2025-03-26 19:24:37 -0400

    bring a few qof opts

--- lua/custom/options/init.lua
+++ lua/custom/options/init.lua
@@ -43,8 +43,41 @@ function M.custom_gdscript()
   end
 end
 
-function M.setup()
+function M.vim_opts()
+  --- general
   vim.opt.relativenumber = true
+  vim.opt.guicursor = ''
+  vim.opt.relativenumber = true
+  vim.opt.scrolloff = 8
+  vim.opt.signcolumn = 'yes'
+  vim.opt.isfname:append '@-@'
+
+  -- fast moving
+  vim.opt.updatetime = 50
+end
+
+function M.keymaps()
+  -- preserve paste buffer
+  vim.keymap.set('x', '<leader>p', '"_dP')
+  vim.keymap.set('n', '<leader>y', '"+y')
+  vim.keymap.set('v', '<leader>y', '"+y')
+  vim.keymap.set('n', '<leader>Y', '"+Y')
+
+  -- removing shit keymaps
+  vim.keymap.set('n', 'Q', '<nop>')
+  vim.keymap.set('n', 's', '<nop>') -- enables mini.surround
+
+  -- quickfix list navigation
+  vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
+  vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
+  vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
+  vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
+end
+
+--- options set up
+function M.setup()
+  M.vim_opts()
+  M.keymaps()
 end
 
 M.setup()
